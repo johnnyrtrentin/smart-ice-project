45489606f29d3c90055dbb2489dfc291b9dcf2b5
Johnny Trentin <johnnyrtrentin@gmail.com>
2020-03-19 00:29:00 -0300

    removed some unecessary routes and add database logic to controller

--- backend/api/controller/apiController.js
+++ backend/api/controller/apiController.js
@@ -3,38 +3,83 @@
 const model = require('../model/apiModel');
 const firestore = require('../../database/firebase');
 
-exports.getUserInfo = function (request, response) {
-    firestore.database().collection("users").get()
-        .then(c => {
-            c.forEach(res =>{
-                response.status(200).json(res.data());
-            });
+const db = firestore.database();
+
+exports.getAllUserInfo = function (request, response) {
+    db.collection('users')
+        .get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+                response.status(404).json({ error: 'No matching collection.' });
+                return;
+            }
+            snapshot.forEach(user =>
+                response.status(200).json(user.data())
+            );
         })
-        .catch(er => console.log(er));
+        .catch(err => response.status(500).json({ error: 'Something wrong.' }));
+};
+
+exports.getUserInfo = function (request, response) {
+    const paramName = request.query.name;
+
+    db.collection('users')
+        .where('name', '==', paramName)
+        .get()
+        .then((snapshot) => {
+            if (snapshot.empty) {
+                response.status(404).json({ error: 'No matching user information.' });
+                return;
+            }
+            snapshot.forEach(doc => 
+                response.status(200).json(doc.data())
+            );
+        })
+        .catch(err => response.status(500).json({ error: 'Something wrong.' }));
 }
 
 exports.postUserInfo = function (request, response) {
-    const json = { "a": "a" };
-    firestore.database().collection("Teste").doc("teste2").set({ json })
-    response.status(200).json({ json });
-}
+    const user = request.body;
+    const personName = user.hasOwnProperty('name') 
+        ? user.name 
+        : undefined;
+
+    db.collection('users').doc(personName)
+        .set(user)
+        .then(() =>
+            response.status(200).json(user))
+        .catch(err => response.status(500).json({ error: 'something wrong.' }));
+};
 
 exports.getUserLocation = function (request, response) {
-    response.status(200).json({});
-}
+    const userLocation = request.query.name;
+    const userName = userLocation.hasOwnProperty('username') 
+        ? userLocation.name 
+        : undefined;
+
+    db.collection('location')
+        .where('username', '==', userName)
+        .get()
+        .then((snapshot) => {
+            if (snapshot.empty) {
+                response.status(404).json({ error: 'No matching user location.' });
+                return;
+            }
+            response.status(200).json();
+
+        })
+        .catch(err =>err => response.status(500).json({ error: 'something wrong.' }));
+};
 
 exports.postUserLocation = function (request, response) {
-    response.status(200).json({});
-}
+    const location = request.body;
+    const userName = location.hasOwnProperty('username')
+        ? location.username 
+        : undefined;
 
-exports.getUserLocation = function (request, response) {
-    response.status(200).json({});
-}
-
-exports.getUserDevice = function (request, response) {
-    response.status(200).json({});
-}
-
-exports.postUserDevvice = function (request, response) {
-    response.status(200).json({});
-}
+    db.collection('location').doc(userName)
+        .set(location)
+        .then(() =>
+            response.status(200).json(location))
+        .catch(err => response.status(500).json({ error: 'something wrong.' }))
+};
--- backend/api/routes/apiRoutes.js
+++ backend/api/routes/apiRoutes.js
@@ -1,19 +1,18 @@
 'use strict';
 
 module.exports = function (app) {
-    var apiController = require('../controller/apiController');
+    const apiController = require('../controller/apiController');
 
     app.route('/api/user')
         .get(apiController.getUserInfo)
         .post(apiController.postUserInfo);
 
+    app.route('/api/user/all')
+        .get(apiController.getAllUserInfo);
+
     app.route('/api/user/location')
         .get(apiController.getUserLocation)
         .post(apiController.postUserLocation);
-
-    app.route('api/user/device')
-        .get(apiController.getUserDevice)
-        .post(apiController.postUserDevvice);
 
     app.use(function (request, response) {
         response.status(404).send({ url: request.originalUrl + ' not found' })
