96f5bf3a4c519a53b2986f0eeacf9b084fd6cad5
Johnny Trentin <johnnyrtrentin@gmail.com>
2020-05-09 14:18:38 -0300

    use cors library

--- src/server.js
+++ src/server.js
@@ -2,17 +2,11 @@ const express  = require('express'),
     bodyParser = require('body-parser'),
     routes     = require('./api/routes/apiRoutes'),
     firebase   = require('./database/firebase'),
-    cors = require('cors'),
+    cors       = require('cors'),
     app        = express(),
     port       = process.env.PORT || 3300;
     
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
     
