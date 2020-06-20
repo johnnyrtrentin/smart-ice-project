7acd2805ada62dd65a638bb0ff5a02458fece8cd
Johnny Trentin <johnnyrtrentin@gmail.com>
2020-06-20 20:54:38 -0300

    add arduinoJson to parse the return of the get request

--- iot/smart-ice-iot/smart-ice-iot.ino
+++ iot/smart-ice-iot/smart-ice-iot.ino
@@ -1,5 +1,8 @@
+
 #include "WiFi.h"
 #include <HTTPClient.h>
+
+#include "ArduinoJson.h"
 
 const char* ssid     = "";
 const char* password =  "";
@@ -11,40 +14,65 @@ unsigned long lastTime   = 0;
 unsigned long timerDelay = 10000;
 
 void setup() {
-
+  setSerialPort();
   connectWiFi();
 }
  
 void loop() {
- verifyArrivalTime();
+ getDeviceStatus();
 }
 
-void verifyArrivalTime() {
+int deserializeRequest(String value) {
+  char json[800];
+  
+  value.replace(" ", "");
+  value.replace("\n", "");
+  value.trim();
+  value.remove(0, 1);
+  value.toCharArray(json, 800);
+  
+  StaticJsonDocument <1024> doc;        
+  deserializeJson(doc, json);
+
+  const char* temperature = doc["temperature"];
+  boolean deviceStatus = doc["status"];
+  
+  Serial.println(String(temperature) + "--" +deviceStatus + "\n");
+
+  return deviceStatus;
+}
+
+void getDeviceStatus() {
   if ((millis() - lastTime) > timerDelay) {
     if (WiFi.status() == WL_CONNECTED) {
     
       HTTPClient http;
   
-      String getAllRoutes = endpointAPI + "user/location/all";
-  
-      http.begin(getAllRoutes.c_str());
+      String getAllDevices = endpointAPI + "device/all";
+
+      http.begin(getAllDevices .c_str());
   
       int httpResponseCode = http.GET();
 
       if (httpResponseCode > 0) {
-        Serial.print("HTTP Response code: ");
+        Serial.print("HTTP status: ");
         Serial.println(httpResponseCode);
-        
+
         String payload = http.getString();
-        Serial.println(payload);
-  
+
+        //device status == true
+        if (deserializeRequest(payload) == 1) {
+          Serial.println("true");
+        } else {
+          Serial.println("false");
+        }
+        
       } else {
-          Serial.print("Error code: ");
-          Serial.println(httpResponseCode);
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
       }
 
       http.end();
-
     } else {
       Serial.println("WiFi Disconnected!");
   }
@@ -54,17 +82,19 @@ void verifyArrivalTime() {
 }
 
 void connectWiFi() {
-  Serial.begin(115200);
-  
   WiFi.begin(ssid, password);
   
-  Serial.print("Connecting");
+  Serial.print("Connecting!");
 
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("......");
+    Serial.println("Trying to connect...");
   }
 
-  Serial.print("Successfully connected in WiFi network with the IP: ");
+  Serial.print("Successfully connected in WiFi with the IP: ");
   Serial.println(WiFi.localIP());
+}
+
+void setSerialPort() {
+  Serial.begin(115200);
 }
